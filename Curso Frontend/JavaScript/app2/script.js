b994cd1fbc3941e641ac8f420419eb456225038c
Jhonnatan Luiz <jhonnatanluiz@yahoo.com.br>
2025-11-15 22:54:00 -0300

    feat: enhance mosquito game with dynamic positioning and life indicators

--- Curso Frontend/JavaScript/app2/script.js
+++ Curso Frontend/JavaScript/app2/script.js
@@ -12,6 +12,11 @@ ajustaTamanhoPalcoJogo();
 
 function posicaoRandomica() {
 
+    //Remover o mosquito anterior (caso exista)
+    if (document.getElementById("mosquito")) {
+        document.getElementById("mosquito").remove();
+    }
+
     var posicaoX = Math.floor(Math.random() * largura) - 90;
     var posicaoY = Math.floor(Math.random() * altura) - 90;
 
@@ -23,13 +28,37 @@ function posicaoRandomica() {
     //Criar o elemento html
     var mosquito = document.createElement("img");
     mosquito.src = "imagens/mosquito.png";
-    mosquito.className = "mosquito1";
+    mosquito.className = tamanhoAleatorio() + " " + ladoAleatorio();
     mosquito.style.left = posicaoX + "px";
     mosquito.style.top = posicaoY + "px";
     mosquito.style.position = "absolute";
+    mosquito.id = "mosquito";
 
     document.body.appendChild(mosquito);
     
+}
+
+function tamanhoAleatorio() { 
+    var classe = Math.floor(Math.random() * 3);
+
+    switch(classe) {
+        case 0:
+            return "mosquito1";
+        case 1:
+            return "mosquito2";
+        case 2:
+            return "mosquito3";
+    }
+}
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2);
+    switch(classe) {
+        case 0:
+            return "ladoA";
+        case 1:
+            return "ladoB";
+    }
 }
 
 
